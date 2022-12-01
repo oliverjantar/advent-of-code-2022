321c71ddc0d5fe5cfa19cc6060cfada756516553
jiriandras <jiriandras@protonmail.com>
2022-12-01 22:26:15 +0100

    Finish day-1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -11,13 +11,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut biggest = 0;
     let mut current = 0;
     for line in FileReader::open("./input", 16)? {
-        // println!("{:?}", line);
-
-        current = match line {
-            Ok(line) => match line.parse() {},
+        match line {
+            Ok(line) => match line[..line.len() - 1].parse::<i32>() {
+                Ok(number) => current += number,
+                Err(_) => current = 0,
+            },
             Err(_) => panic!("error while reading file"),
         }
+
+        if current > biggest {
+            biggest = current;
+        }
     }
+
+    println!("biggest: {}", biggest);
 
     Ok(())
 }
