c394c00523c3d6bc88b66a84ad6348bce720a3fe
jiriandras <jiriandras@protonmail.com>
2022-12-01 23:00:20 +0100

    Add second task of day 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,13 +1,37 @@
-use {
-    shared::file_reader::FileReader,
-    std::{
-        error::Error,
-        fs::File,
-        io::{self, prelude::*, BufReader},
-    },
-};
+use {shared::file_reader::FileReader, std::error::Error};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
+    task_1().unwrap();
+    task_2().unwrap();
+}
+
+fn task_2() -> Result<(), Box<dyn Error>> {
+    let mut data = Vec::new();
+
+    let mut current = 0;
+    for line in FileReader::open("./input", 16)? {
+        match line {
+            Ok(line) => match line[..line.len() - 1].parse::<i32>() {
+                Ok(number) => current += number,
+                Err(_) => {
+                    data.push(current);
+                    current = 0
+                }
+            },
+            Err(_) => panic!("error while reading file"),
+        }
+    }
+    data.push(current);
+
+    data.sort();
+    data.reverse();
+
+    println!("biggest: {}", data[0] + data[1] + data[2]);
+
+    Ok(())
+}
+
+fn task_1() -> Result<(), Box<dyn Error>> {
     let mut biggest = 0;
     let mut current = 0;
     for line in FileReader::open("./input", 16)? {
@@ -24,7 +48,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    println!("biggest: {}", biggest);
+    println!("sum of top three: {}", biggest);
 
     Ok(())
 }
