81df75e988eaf15a7e03ec5c01bfe9d5565b0605
jiriandras <jiriandras@protonmail.com>
2022-12-03 21:03:27 +0100

    Finish task1 of day3

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,10 +1,96 @@
+use std::error::Error;
+
 use shared::file_reader::FileReader;
+
 fn main() {
-    println!("Hello, world!");
+    task_1().unwrap();
 }
 
-fn apply_fn(rucksack: &str) -> char {
-    'a'
+fn task_1() -> Result<(), Box<dyn Error>> {
+    let mut sum = 0;
+    for line in FileReader::open("./input", 200)? {
+        if let Ok(line) = line {
+            let line = line.trim_end();
+
+            let res = apply_fn(line);
+            if let Some(res) = res {
+                let point = get_point(res);
+                sum += point;
+            }
+        }
+    }
+    println!("{}", sum);
+    Ok(())
+}
+
+fn apply_fn(rucksack: &str) -> Option<char> {
+    let right = &rucksack[..rucksack.len() / 2];
+    let left = &rucksack[rucksack.len() / 2..];
+
+    for c in right.chars() {
+        if left.contains(c) {
+            return Some(c);
+        }
+    }
+    None
+}
+
+fn get_point(c: char) -> i32 {
+    match c {
+        'a' => 1,
+        'b' => 2,
+        'c' => 3,
+        'd' => 4,
+        'e' => 5,
+        'f' => 6,
+        'g' => 7,
+        'h' => 8,
+        'i' => 9,
+        'j' => 10,
+        'k' => 11,
+        'l' => 12,
+        'm' => 13,
+        'n' => 14,
+        'o' => 15,
+        'p' => 16,
+        'q' => 17,
+        'r' => 18,
+        's' => 19,
+        't' => 20,
+        'u' => 21,
+        'v' => 22,
+        'w' => 23,
+        'x' => 24,
+        'y' => 25,
+        'z' => 26,
+        'A' => 27,
+        'B' => 28,
+        'C' => 29,
+        'D' => 30,
+        'E' => 31,
+        'F' => 32,
+        'G' => 33,
+        'H' => 34,
+        'I' => 35,
+        'J' => 36,
+        'K' => 37,
+        'L' => 38,
+        'M' => 39,
+        'N' => 40,
+        'O' => 41,
+        'P' => 42,
+        'Q' => 43,
+        'R' => 44,
+        'S' => 45,
+        'T' => 46,
+        'U' => 47,
+        'V' => 48,
+        'W' => 49,
+        'X' => 50,
+        'Y' => 51,
+        'Z' => 52,
+        _ => panic!("invalid character"),
+    }
 }
 
 #[cfg(test)]
@@ -28,8 +114,11 @@ mod tests {
 
         let data = vec![r1, r2, r3, r4, r5, r6];
 
+        let mut sum = 0;
         for rucksack in data {
-            assert_eq!(apply_fn(rucksack.0), rucksack.1);
+            assert_eq!(apply_fn(rucksack.0), Some(rucksack.1));
+            sum += get_point(rucksack.1);
         }
+        assert_eq!(sum, 157)
     }
 }
