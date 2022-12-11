e3af8bcf278384a500914cda79ca61aa22571346
jiriandras <jiriandras@protonmail.com>
2022-12-11 13:23:37 +0100

    Finished day 10

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,5 +1,6 @@
 mod task1;
-
+mod task2;
 fn main() {
     task1::run().unwrap();
+    task2::run().unwrap();
 }
--- day-10/src/task2.rs
+++ day-10/src/task2.rs
@@ -3,23 +3,23 @@ use std::error::Error;
 use shared::file_reader::FileReader;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let mut cycle = 0;
-    let mut reg = 1;
+    let mut cycle: usize = 0;
+    let mut reg: isize = 1;
     let mut wait = 0;
     let mut add_to_reg = 0;
     let mut file_iterator = FileReader::open("./input", 15)?;
 
-    let mut total_sum = 0;
+    let mut crt: Vec<char> = vec![];
+
     loop {
         cycle += 1;
 
-        match cycle {
-            20 | 60 | 100 | 140 | 180 | 220 => {
-                total_sum += cycle * reg;
-                println!("{}", total_sum);
-            }
-            _ => {}
-        };
+        if reg <= cycle as isize && reg + 2 >= cycle as isize {
+            crt.push('#');
+        } else {
+            crt.push('.');
+        }
+
         if wait == 0 {
             if let Some(line) = file_iterator.next() {
                 if let Ok(line) = line {
@@ -28,7 +28,7 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                     match line[0] {
                         "noop" => {}
                         "addx" => {
-                            add_to_reg = line[1].parse::<i32>().unwrap();
+                            add_to_reg = line[1].parse::<isize>().unwrap();
 
                             wait = 1;
                         }
@@ -44,8 +44,13 @@ pub fn run() -> Result<(), Box<dyn Error>> {
             wait -= 1;
             reg += add_to_reg;
         }
+
+        if cycle == 40 {
+            println!("{:?}", &crt[0..39]);
+            cycle = 0;
+            crt = vec![];
+        }
     }
 
-    println!("{}", total_sum);
     Ok(())
 }
